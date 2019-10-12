4a84dc72d8884a24239edda63bcf2c980136db92
Akash Jobanputra <akash.jobanputra@hotmail.com>
2019-10-12 23:25:40 +0530

    added migrations folder to git

---
+++ migrations/versions/ad4d4f9436cc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ad4d4f9436cc
+Revises: 
+Create Date: 2019-10-12 19:52:57.811041
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ad4d4f9436cc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
