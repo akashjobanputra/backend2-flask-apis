be7dd5a52fee2a7953479ba0ac3d242490314cd5
Akash Jobanputra <akash.jobanputra@hotmail.com>
2019-10-14 01:30:55 +0530

    updated get_clusters to return single cluster on cluster_id provided

--- app/api/clusters.py
+++ app/api/clusters.py
@@ -3,17 +3,26 @@ from ..models import Cluster
 from .. import db
 from . import api
 
-@api.route('/clusters/')
-def get_clusters():
-    clusters = Cluster.query.all()
-    return jsonify({
-        'clusters': [cluster.to_json() for cluster in clusters]
-    })
 
-@api.route('/clusters/', methods=['POST'])
+@api.route('/clusters', defaults={'cluster_id': None})
+@api.route('/clusters/<int:cluster_id>')
+def get_clusters(cluster_id):
+    if cluster_id:
+        clusters = Cluster.query.filter_by(id=cluster_id).first_or_404()
+        response = {
+            'cluster': clusters.to_json()
+        }
+    else:
+        clusters = Cluster.query.all()
+        response = {
+            'clusters': [cluster.to_json() for cluster in clusters]
+        }
+    return jsonify(response)
+
+
+@api.route('/clusters', methods=['POST'])
 def new_cluster():
     cluster = Cluster.from_json(request.json)
     db.session.add(cluster)
     db.session.commit()
     return jsonify(cluster.to_json()), 201
-
