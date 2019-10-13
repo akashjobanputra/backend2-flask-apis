e826561f65ffa343ea7e612537d36deece07016d
Akash Jobanputra <akash.jobanputra@hotmail.com>
2019-10-13 13:43:11 +0530

    configured flask-migrate

--- backend2.py
+++ backend2.py
@@ -1,14 +1,12 @@
 import os
-from flask_script import Manager
 from flask_migrate import Migrate, upgrade, MigrateCommand
 from app import create_app, db
+from app.models import Machine, Tag, Cluster    # not used in this file, but still required so flask_migrate can work
 
 f_app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(f_app, db)
-manager = Manager(f_app)
 
-manager.add_command('db', MigrateCommand)
-
+f_app.cli.add_command('db', MigrateCommand)
 
 @f_app.cli.command('deploy')
 def upgrade_migrate():
