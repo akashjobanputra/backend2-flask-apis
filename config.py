67eb2aee69580680ffe491fa9adb60cd43755b71
Akash Jobanputra <akash.jobanputra@hotmail.com>
2019-10-14 23:29:06 +0530

    TestDBInMem

--- config.py
+++ config.py
@@ -21,8 +21,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-    #     'sqlite://'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class ProductionConfig(Config):
     DEBUG = False
